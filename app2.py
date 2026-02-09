d584ae05be23671b394b3bcad8c68f95af2bd925
st-flashcard <beginner.speech.therapist1112@gmail.com>
2026-02-10 04:35:58 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -68,11 +68,16 @@ st.markdown("""
 # 3. データの準備
 # ----------------------------------------
 course_basic = [
-    {"filename": "apple.jpg", "answer": "りんご"},
+   {"filename": "apple.jpg", "answer": "りんご"},
     {"filename": "cat.jpg",   "answer": "ねこ"},
     {"filename": "pen.jpg",   "answer": "ぺん"},
-    {"filename": "watch.jpg", "answer": "とけい"},
-    {"filename": "book.jpg",  "answer": "ほん"},
+    {"filename": "dog.jpg",    "answer": "いぬ"},    # 追加！
+    {"filename": "book.jpg",   "answer": "ほん"},    # 追加！
+    {"filename": "car.jpg",    "answer": "くるま"},  # 追加！
+    {"filename": "flower.jpg", "answer": "はな"},    # 追加！
+    {"filename": "fish.jpg",   "answer": "さかな"},  # 追加！
+    {"filename": "bird.jpg",   "answer": "とり"},    # 追加！
+    {"filename": "shoe.jpg",   "answer": "くつ"},    # 追加！
 ]
 
 course_animals = [
