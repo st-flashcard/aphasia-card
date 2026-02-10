8c898fbb28b177759fbd0d844b91dd25650d3cd9
st-flashcard <beginner.speech.therapist1112@gmail.com>
2026-02-10 12:08:07 +0900

    Update data.py

--- data.py
+++ data.py
@@ -14,16 +14,16 @@ course_animals1 = [
 ]
 
 course_animals2 = [
-    {"filename": "dog.jpg",      "answer": "いぬ"},
-    {"filename": "cat.jpg",      "answer": "ねこ"},
-    {"filename": "panda.jpg",    "answer": "ぱんだ"},
-    {"filename": "lion.jpg",     "answer": "らいおん"},
-    {"filename": "giraffe.jpg",  "answer": "きりん"},
-    {"filename": "elephant.jpg", "answer": "ぞう"},
-    {"filename": "koala.jpg",    "answer": "こあら"},
-    {"filename": "gorilla.jpg",  "answer": "ごりら"},
-    {"filename": "penguin.jpg",  "answer": "ぺんぎん"},
-    {"filename": "tiger.jpg",    "answer": "とら"},
+    {"filename": "bear.jpg",     "answer": "くま"},
+    {"filename": "monkey.png",   "answer": "さる"},
+    {"filename": "deer.jpg",     "answer": "しか"},
+    {"filename": "owl.jpg",      "answer": "ふくろう"},
+    {"filename": "zebra.jpg",    "answer": "しまうま"},
+    {"filename": "kangaroo.jpg", "answer": "かんがるー"},
+    {"filename": "squirrel.png", "answer": "りす"},
+    {"filename": "pig.png",      "answer": "ぶた"},
+    {"filename": "sheep.png",    "answer": "ひつじ"},
+    {"filename": "rabbit.png",   "answer": "うさぎ"},
 ]
 
 course_animals3 = [
@@ -37,4 +37,14 @@ course_animals3 = [
     {"filename": "gorilla.jpg",  "answer": "ごりら"},
     {"filename": "penguin.jpg",  "answer": "ぺんぎん"},
     {"filename": "tiger.jpg",    "answer": "とら"},
+    {"filename": "bear.jpg",     "answer": "くま"},
+    {"filename": "monkey.png",   "answer": "さる"},
+    {"filename": "deer.jpg",     "answer": "しか"},
+    {"filename": "owl.jpg",      "answer": "ふくろう"},
+    {"filename": "zebra.jpg",    "answer": "しまうま"},
+    {"filename": "kangaroo.jpg", "answer": "かんがるー"},
+    {"filename": "squirrel.png", "answer": "りす"},
+    {"filename": "pig.png",      "answer": "ぶた"},
+    {"filename": "sheep.png",    "answer": "ひつじ"},
+    {"filename": "rabbit.png",   "answer": "うさぎ"},
 ]
